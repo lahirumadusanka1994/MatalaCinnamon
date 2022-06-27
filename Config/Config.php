37d8d2d04c23ba5920b35d837569df60c68587b6
lahirumadusanka1994 <mlahiru133@gmail.com>
2022-06-28 01:47:54 +0530

    a

--- Config/Config.php
+++ Config/Config.php
@@ -1,21 +1,31 @@
 <?php
 
+
+//define('SUB_DIRECTORY','MatalaCinnamon'); // Mahesh
+define('SUB_DIRECTORY','matalacinnamon'); // my
+//define('SUB_DIRECTORY',''); // live
+
+
 define('SITE_URL',$_SERVER['HTTP_HOST'] );
-define('SUB_DIRECTORY','MatalaCinnamon');
+
 define('SITE_ROOT',$_SERVER['DOCUMENT_ROOT'] .'/'.SUB_DIRECTORY.'/');
 
 define('CONFIG_DIR',$_SERVER['DOCUMENT_ROOT'] .SUB_DIRECTORY. '/Config/');
 
+
+
+
 // Db Credentials
 // live
-define('DB_HOST','localhost');
-define('DB_USER','mathala_mysql');
-define('DB_PASSWORD','Mathala@123');
-define('DB_NAME','mathala_mysql');
+
+// define('DB_HOST','localhost');
+// define('DB_USER','mathala_mysql');
+// define('DB_PASSWORD','Mathala@123');
+// define('DB_NAME','mathala_mysql');
 
 // local
-//define('DB_HOST','localhost');
-//define('DB_USER','root');
-//define('DB_PASSWORD','');
-//define('DB_NAME','mathala_mysql');
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+define('DB_NAME','mathala_mysql');
 
