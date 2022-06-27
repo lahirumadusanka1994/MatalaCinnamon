bf0a26454d21fef4712ee300aeff65757d55d30a
MaheshBro <maheshkcot95@gmail.com>
2022-06-27 23:59:46 +0530

    added contact form options

--- Config/Config.php
+++ Config/Config.php
@@ -9,14 +9,14 @@ define('CONFIG_DIR',$_SERVER['DOCUMENT_ROOT'] .SUB_DIRECTORY. '/Config/');
 
 // Db Credentials
 // live
-//define('DB_HOST','localhost');
-//define('DB_USER','mathala_mysql');
-//define('DB_PASSWORD','Mathala@123');
-//define('DB_NAME','mathala_mysql');
+define('DB_HOST','localhost');
+define('DB_USER','mathala_mysql');
+define('DB_PASSWORD','Mathala@123');
+define('DB_NAME','mathala_mysql');
 
 // local
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASSWORD','');
-define('DB_NAME','mathala_mysql');
+//define('DB_HOST','localhost');
+//define('DB_USER','root');
+//define('DB_PASSWORD','');
+//define('DB_NAME','mathala_mysql');
 
