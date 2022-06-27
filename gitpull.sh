c65807da71a865836254dfb9a5c84fdee6df6d43
MaheshBro <maheshkcot95@gmail.com>
2022-06-28 02:39:45 +0530

    added contact form options

--- gitpull.sh
+++ gitpull.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git pull https://lahirumadusanka1994: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@github.com/lahirumadusanka1994/MatalaCinnamon.git
+git clone https://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@github.com/lahirumadusanka1994/MatalaCinnamon.git main
 
